73c817189a63ec13a3feef7649aecce9a7c9783f
Abdullah Abu Sayed <abdullahabusayedrafi@gmail.com>
2026-07-15 21:29:37 +0600

    Updated day1 code

--- Python_Fundamentals/day1_print_and_variables.py
+++ Python_Fundamentals/day1_print_and_variables.py
@@ -1,4 +1,4 @@
-'''print("my name is rafi \nI am 19 years old", "yfvfhsgcv")
+print("my name is rafi \nI am 19 years old", "yfvfhsgcv")
 
 num1 = 90
 num2 = 9
@@ -11,17 +11,17 @@ b= 3.22
 c= int( a/b)
 print (c)
 y="18787"
-print (y,type(int(y)))'''
+print (y,type(int(y)))
 
 #avg of 2 numbers
-'''a=float(input("Number 1"))
+a=float(input("Number 1"))
 b=float(input("number 2"))
 c =float((a+b)/2)
 print("average of 2 numbers is", c)
 
 d= 2
 d+=5
-print(d)'''
+print(d)
 
 '''x = 10 + 3 * 2 ** 2 # 1st 2 ** 2 = 4, 3 * 4 = 12, 10 + 12 = 22
 print(x) '''
