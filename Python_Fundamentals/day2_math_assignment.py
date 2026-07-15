ae94ca45041493ee2f1d49f8054287985c1562f7
Abdullah Abu Sayed <abdullahabusayedrafi@gmail.com>
2026-07-15 21:40:20 +0600

    Updated day 2 codes

--- Python_Fundamentals/day2_math_assignment.py
+++ Python_Fundamentals/day2_math_assignment.py
@@ -1,37 +1,37 @@
-# # Q1
+# Q1
 
-# salary = int(input("Enter Your Salary :"))
-# if (salary <30000 ):
-#     print("5%")
-# elif (salary>= 30000 and salary<= 70000):
-#     print("15%")
-# else:
-#     print("25%")
+salary = int(input("Enter Your Salary :"))
+if (salary <30000 ):
+    print("5%")
+elif (salary>= 30000 and salary<= 70000):
+    print("15%")
+else:
+    print("25%")
 
-# # Q2
-# def fun_even(a,b):
-#     for i in range(a,b+1):
-#         if (i%2==0):
-#             print(i)
+# Q2
+def fun_even(a,b):
+    for i in range(a,b+1):
+        if (i%2==0):
+            print(i)
 
-# x = int(input("A :"))
-# y = int(input("B : "))
-# fun_even(x,y)
+x = int(input("A :"))
+y = int(input("B : "))
+fun_even(x,y)
 
-##Q4
-# def count_digits(n):
+#Q4
+def count_digits(n):
    
         
-#     count = 0
-#     while n > 0:
-#         count += 1
-#         n = n // 10  # Remove the rightmost digit
+    count = 0
+    while n > 0:
+        count += 1
+        n = n // 10  # Remove the rightmost digit
         
-#     return count
+    return count
 
-# # Example usage:
-# num = int(input("Enter a number: "))
-# print("Number of digits:", count_digits(num))
+# Example usage:
+num = int(input("Enter a number: "))
+print("Number of digits:", count_digits(num))
 
 ##Q7
 while  True:
--- Python_Fundamentals/day2_type_casting_and_inputs_Functions.py
+++ Python_Fundamentals/day2_type_casting_and_inputs_Functions.py
@@ -1,16 +1,15 @@
 #Conditional Styatements
-'''
 a = int(input("Entetr your age: "))
 if a>18 :
     print ("You are adult")
 elif a==18:
     print("You atre 18")
 else:
-    print ("You are not adult")'''
+    print ("You are not adult")
 
 #if elif
 
-''''color_input = input("Enter the color (Red, Green, Yellow): ")
+color_input = input("Enter the color (Red, Green, Yellow): ")
 
 if color_input== "Red":
     print("You selected Red")
@@ -20,8 +19,8 @@ elif color_input== "Yellow":
     print("You selected Yellow")
 else:
     print("Invalid color selected")
-    '''
-''''num = int(input("Enter your number : "))
+    
+num = int(input("Enter your number : "))
 
 # Multiple by 5
 
@@ -30,20 +29,19 @@ if num==5:
 elif num%5==0:
     print ("Multiple of 5")
 else :
-    print("Number is not a multiple of 5 and not 5")'''
+    print("Number is not a multiple of 5 and not 5")
 
 #Even Odd
 
-''''n = int(input("enter a number"))
+n = int(input("enter a number"))
 if (n%2==0):
     print("Devided by 2")
 else:
     print("not devided by 2")
-'''
 
 
 # Nesting
-'''
+
 username = input("Enter your username :")
 password = input("Enter Your Pass :")
 
@@ -56,11 +54,11 @@ else:
         print("Wrong username")
     else:
         print("wrong pass ")
-'''
+
 
 #Loop
 #while
-'''
+
 i=1
 while (i<=5):
     print("Hello World" , i)
@@ -73,70 +71,69 @@ while (i>=1):
     print(i)
     
     i -=1
-'''
 
-## Print multiplication
-# n = int(input("Enter the number you want to multiply :"))
+# Print multiplication
+n = int(input("Enter the number you want to multiply :"))
 
-# i=0
-# while (i<10):
-#     print (n, "*" ,i+1,"= ", n*(i+1) )
-#     i +=1
+i=0
+while (i<10):
+    print (n, "*" ,i+1,"= ", n*(i+1) )
+    i +=1
 
-# Break and Continue
+## Break and Continue
 
-# i = 1
-# while (i <= 10):
-#     if (i % 2 == 0):
+i = 1
+while (i <= 10):
+    if (i % 2 == 0):
         
-#         continue
-#     print(i)
-#     i += 1
+        continue
+    print(i)
+    i += 1
 
 
-#### For Loop
+### For Loop
 
-# x = "Hello World"
+x = "Hello World"
 
-# for i in x:
-#     print (i)
+for i in x:
+    print (i)
 
-# word ="ioishigvbiugvbiusg iyfydu giouf wyiusvhj giiiiiiiiiiiiiiiif utidyscyugukigf dscduyxkjgbjf aop;sh"
+word ="ioishigvbiugvbiusg iyfydu giouf wyiusvhj giiiiiiiiiiiiiiiif utidyscyugukigf dscduyxkjgbjf aop;sh"
 
-# count = 0
-# for i in word:
-#     if (i=="i"):
-#      count += 1
+count = 0
+for i in word:
+    if (i=="i"):
+     count += 1
 
-# print (count)
+print (count)
 
-# for c in range(10):
-#     print(c+1) 
+for c in range(10):
+    print(c+1) 
 
-# for ch in word:
-#     if (ch =="a" or "e" or "i" ):
-#         count +=1
+for ch in word:
+    if (ch =="a" or "e" or "i" ):
+        count +=1
 
-# print (count)
+print (count)
 
 
 
-### sum of n
+## sum of n
 
-# count = 0
-# for i in range(1,6,2):
-#      count += i
+count = 0
+for i in range(1,6,2):
+     count += i
 
-# print(count)
+print(count)
 
-# def sum(a,b,c):
-#     s= (a+b+c)/3
+def sum(a,b,c):
+    s= (a+b+c)/3
 
-#     print(s)
+    print(s)
 
-# sum(2,9,4)
+sum(2,9,4)
 
-### fact function
+## fact function
 
 def calc(n):
     fact=1
