ad2576109647295f158af6f3c43a22d2e1821a1f
Abdullah Abu Sayed <abdullahabusayedrafi@gmail.com>
2026-07-22 23:47:38 +0600

    Learning about file io and manupulation

---
+++ Python_Fundamentals/file_operations.py
@@ -0,0 +1,46 @@
+#### Read
+
+# f = open("Python_Fundamentals/sample.txt", "r")
+# Data = f.read()
+# print(Data)
+# Data=f.readline()
+# print(Data)
+
+# f.close
+
+###Write
+
+# wr=open("Python_Fundamentals/sample.txt", "w")
+# new=wr.write("Thsi is overwrited \n New data added")
+# wr.close()
+
+### Append is also same just write at the end
+
+#### w+, r+, a+
+
+### with kw automatically handles open close
+
+# with open("Python_Fundamentals/sample.txt", "r+") as e:
+#     # e.write("New ")
+#     # print(e.read()) ## pointer is in the beginning
+
+#     e.read()
+#     e.write("This is a trial")## pointer is at the end so its write last
+
+# #### similarly works with a+ and w+(works with pointer)
+
+## import os
+# # y = open("Python_Fundamentals/sample2.txt", "x")
+
+
+# ## delete
+## os.remove("Python_Fundamentals/sample2.txt")
+
+
+z = open("Python_Fundamentals/sample.txt", "r")
+data ="python"
+while True:
+    if data in z:
+        k =z.readline()
+        print(k)
+
