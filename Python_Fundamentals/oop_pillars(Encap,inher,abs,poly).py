63b3a72aa4a394dce2a30ed088790f1f548b439e
Abdullah Abu Sayed <abdullahabusayedrafi@gmail.com>
2026-07-23 03:21:23 +0600

    Updated Python_Fundamentals/oop_pillars(Encap,inher,abs,poly).py

--- Python_Fundamentals/oop_pillars(Encap,inher,abs,poly).py
+++ Python_Fundamentals/oop_pillars(Encap,inher,abs,poly).py
@@ -1,84 +1,84 @@
-#### Encaptulation - Wrapping data and functions into a single unit
+### Encaptulation - Wrapping data and functions into a single unit
 
-# ### data hiding
-# class BankAccount:
-#     def __init__(self,Name,Balance):
+### data hiding
+class BankAccount:
+    def __init__(self,Name,Balance):
         
-#         self.Name=Name
-#         self.__Balance=Balance ##_ means Protected and __ means private(have to make getter and setter)
+        self.Name=Name
+        self.__Balance=Balance ##_ means Protected and __ means private(have to make getter and setter)
 
-#     def get_balance(self):  ##getter
-#         return self.__Balance
-#     def set_balance(self,newBalance):
-#         self.__Balance=newBalance
+    def get_balance(self):  ##getter
+        return self.__Balance
+    def set_balance(self,newBalance):
+        self.__Balance=newBalance
 
-# acc1=BankAccount("mr. No Name", 100000)
+acc1=BankAccount("mr. No Name", 100000)
 
-# acc1.set_balance(200000)
-# print(acc1.Name,acc1.get_balance())
+acc1.set_balance(200000)
+print(acc1.Name,acc1.get_balance())
 
-# ## Another way to access the private data
-# print(acc1.Name,acc1._BankAccount__Balance)##without the get set
+## Another way to access the private data
+print(acc1.Name,acc1._BankAccount__Balance)##without the get set
 
 
 
-#### Inheritance - reuseing attr and methods from parent or base class
+### Inheritance - reuseing attr and methods from parent or base class
 
-# ###single level inheritance
-# class Employee:
-#     st_time = "10 am"
-#     end_time = "6 pm"
-#     def cng_time(self,new_time):
-#         self.end_time=new_time
+###single level inheritance
+class Employee:
+    st_time = "10 am"
+    end_time = "6 pm"
+    def cng_time(self,new_time):
+        self.end_time=new_time
         
-#     def break_time(self,b_time):
-#         self.b_time=b_time
+    def break_time(self,b_time):
+        self.b_time=b_time
 
 
-# class Teacher(Employee):
-#     def __init__(self,subject):
-#         self.subject=subject
+class Teacher(Employee):
+    def __init__(self,subject):
+        self.subject=subject
 
-# t1=Teacher("english")
-# t1.cng_time("7 pm")
-# t1.break_time("3 pm")
-# print(t1.subject,t1.end_time,t1.b_time)
+t1=Teacher("english")
+t1.cng_time("7 pm")
+t1.break_time("3 pm")
+print(t1.subject,t1.end_time,t1.b_time)
 
-# ### Types of inheritance
+### Types of inheritance
 
-# ## single level inheritance (a to b)- already learned
+## single level inheritance (a to b)- already learned
 
-# ## Multilevel inheritance (a to b to c)
+## Multilevel inheritance (a to b to c)
 
-# ### Multiple Inheritance (multiple parent one child a and b to c)
+### Multiple Inheritance (multiple parent one child a and b to c)
 
-# ## Multilevel inheritance
-# class Assistant_Teacher(Teacher):
-#     def __init__(self, salary,subject):
-#         super().__init__(subject) ## For calling the constractor of the mother class
-#         self.salary= salary
+## Multilevel inheritance
+class Assistant_Teacher(Teacher):
+    def __init__(self, salary,subject):
+        super().__init__(subject) ## For calling the constractor of the mother class
+        self.salary= salary
 
-# assis1=Assistant_Teacher(25000,"PL")
-# print(assis1.salary,assis1.end_time,assis1.subject)
+assis1=Assistant_Teacher(25000,"PL")
+print(assis1.salary,assis1.end_time,assis1.subject)
 
 
-# ### Multiple Inheritance
-# class Student:
-#     def __init__(self,cgpa):
-#         self.cgpa = cgpa
+### Multiple Inheritance
+class Student:
+    def __init__(self,cgpa):
+        self.cgpa = cgpa
     
-# class Tc:
-#     def __init__(self,task):
-#         self.task=task
+class Tc:
+    def __init__(self,task):
+        self.task=task
 
-# class Captain(Student,Tc):
-#     def __init__(self,cgpa,task,name):
-#         super().__init__(cgpa)
-#         Tc.__init__(self,task)        
-#         self.name=name
+class Captain(Student,Tc):
+    def __init__(self,cgpa,task,name):
+        super().__init__(cgpa)
+        Tc.__init__(self,task)        
+        self.name=name
     
-# cap1=Captain(3.14,"Nothingg","Asad")
-# print(cap1.name,cap1.cgpa,cap1.task)
+cap1=Captain(3.14,"Nothingg","Asad")
+print(cap1.name,cap1.cgpa,cap1.task)
 
 
 
